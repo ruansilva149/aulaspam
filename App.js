cc52556f090ffb07e0f67596eab64eb5cf44b990
Ruan Silva <ruan.silva149@etec.sp.gov.br>
2024-09-24 22:04:42 -0300

    style_chat

--- App.js
+++ App.js
@@ -1,29 +1,100 @@
 import React, {useEffect, useState} from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 import socket  from "./socket";
-import Message from "./src/components/Message";
+import { TextInput } from "react-native-web";
 
 export default function App() {
-  const [message, setMessage] = useState('');
+  
+  const [room, setRoom] = useState('default');
+  const [message, setMessage] = useState ('');
+  const [receivedMessage, setReceivedMessage] = useState ('');
+  const sendMessage = () => {
+    socket.emit('send_message', {room, message})
+    setMessage('');
+  };
 
   useEffect(() => {
-    socket.on('connect', () => {
-      console.log("Conectado ao servidor!");
-    });
+    socket.emit('join_room', room)
 
-    socket.on('message', (msg) => {
-      setMessage(msg)
-    });
+    socket.on('receive_message', (msg) =>{
+      setReceivedMessage(msg)
+    })
 
-    return () => {
-      socket.off('connect')
-      socket.off('message')
+    return () =>{
+      socket.off('receive_message')
     }
-  }, []);
-  
+  }, [room])
+
   return (
-  <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <Message></Message>
+    <View style={styles.container}>
+      <Text style={styles.title}>Canal: {room}</Text>
+
+      <TextInput style={styles.input}
+        placeholder="Digite sua mensagem"
+        value={message}
+        onChangeText={setMessage}
+      />
+
+      <Pressable style={styles.button} onPress={sendMessage}>
+        <Text style={styles.buttonText}>Enviar mensagem</Text>
+      </Pressable>
+
+      <Text style={styles.receivedMessageTitle}>Mensagem recebida:</Text>
+      <Text style={styles.receivedMessage}>{ receivedMessage || 'Nenhuma mensagem recebida'}</Text>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#f4f4f4',
+    padding: 20,
+    justifyContent: 'center'
+  },
+  title:{
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center'
+
+  },
+  input:{
+    height: 50,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20,
+    backgroundColor: '#ffff'
+  },
+  button:{
+    backgroundColor: '#4CAF50',
+    paddingVertical: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  buttonText:{
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  receivedMessageTitle:{
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 20,
+    textAlign: 'center'
+  },
+  receivedMessage:{
+    fontSize: 16,
+    color: '#333',
+    marginTop: 10,
+    padding: 10,
+    backgroundColor: '#e8e8e8',
+    borderRadius: 5,
+    textAlign: 'center'
+  }
+
+})
